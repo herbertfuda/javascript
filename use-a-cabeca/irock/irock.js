a1c46aa20d7a5a9da6c88a7118198722651be9eb
herbertfuda <souza.hf@outlook.com>
2022-03-14 10:10:19 -0300

    estudos

--- use-a-cabeca/irock/irock.js
+++ use-a-cabeca/irock/irock.js
@@ -1,13 +1,20 @@
+var nomeUsuario
 function msg(){
     alert('Olá, bem vindo ao site da pedra tosca!')
 }
+function resize(){
+    document.getElementById("irock").style.height = (document.body.clientHeight - 100) * 0.9
+}
 function touchrock(){
-    var nomeUsuario = prompt('Qual o seu nome?')
     if(nomeUsuario){
-        alert(`Olá ${nomeUsuario}, você clicou mesmo em uma pedra? Você consegue ser mais tosco que a Pedra!!!`)
+        alert(`Eu gosto de atenção ${nomeUsuario}. Obrigado!`)
         document.getElementById("irock").src="rock_happy.png"
     }
     else{
-        alert('Você cancelou, parabens!')
+        nomeUsuario = prompt('Qual o seu nome?')
+        alert(`Olá ${nomeUsuario}, você clicou mesmo em uma pedra? Você consegue ser mais tosco que a Pedra!!!`)
+        document.getElementById("irock").src="rock_happy.png"
     }
+    setTimeout("document.getElementById('irock').src = 'rock.png'", 3000)
 }
+
